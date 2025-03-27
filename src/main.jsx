60924dac32c4719b60eb3b63adaaa5714b8919b0
kumari neha rani <1.2ranineha456@gamil.com>
2025-03-27 08:47:00 +0530

    added reactrouter dom

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+} else {
+  console.error("No root element found. Please ensure there's a <div id='root'></div> in your index.html.");
+}
